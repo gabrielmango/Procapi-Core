8451128ca6ff80e0e23014222621ee9a9fa6c6fa
gabrielhmango <gabrielhmango@gmail.com>
2025-10-07 11:39:49 -0300

    feat: chamada da classe CollectionAviso e consulta de numero de processo

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+from src.modulos.collection_aviso import CollectionAviso
 from src.utils.logger_manager import LoggerManager
 
-logger = LoggerManager('example')
+logger = LoggerManager('consulta_avisos_por_processo')
 
 
 @logger.log_execution
 def main():
-    logger.logger.info('Running process...')
+    processos = ['10002018920258130024']
+
+    collection_aviso = CollectionAviso(logger=logger, ambiente='prod')
+
+    for processo in processos:
+        info = collection_aviso.consulta_avisos_por_processo(processo)
+        logger.logger.info(f"Numero aviso: {info['numero']} - situacao: {info['situacao']}")
 
 
 if __name__ == '__main__':
---
+++ src/modulos/collection_aviso.py
@@ -0,0 +1,7 @@
+class CollectionAviso:
+    def __init__(self, logger: str, ambiente: str):
+        self._logger = logger
+        self._ambiente = ambiente
+
+    def consulta_avisos_por_processo(self, numero_processo):
+        pass
